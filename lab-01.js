140ef5d861308499323236f0dc14aa33aa5fc7ba
phatloso <tranthanhphat2503@gmail.com>
2023-09-20 19:43:52 +0700

    a

---
+++ lab-01.js
@@ -0,0 +1,42 @@
+// bai 1
+// cau a
+//console.log('Hello World')
+// cau b
+//console.log("Tran Thanh Phat")
+// cau c
+//console.clear()
+// cau d
+//console.log('*****')
+//console.log('*****')
+//console.log('*****')
+//console.log('*****')
+//console.log('*****')
+// cau e
+//console.log('     *     ')
+//console.log('    ***    ')
+//console.log('   *****   ')
+//console.log('  *******  ')
+//console.log(' ********* ')
+//console.log('***********')
+// cau f
+//console.log(' ** ** ')
+//console.log('*  *  *')
+//console.log('*     *')
+//console.log(' *   * ')
+//console.log('  * *  ')
+//console.log('   *   ')
+// bai 2
+//cau a
+let a = 10;
+// cau b
+let b = '10';
+console.log(b)
+// cau c
+a =20;
+console.log(a)
+// cau d
+const pi=3.14;
+console.log(pi)
+// cau e
+pi =5;
+console.log('pi')

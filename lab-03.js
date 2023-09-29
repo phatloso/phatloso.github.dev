9e90bdfea06670b93cd987710d6bf65f2b94baee
phatloso <tranthanhphat2503@gmail.com>
2023-09-29 19:44:47 +0700

    phat

---
+++ lab-03.js
@@ -0,0 +1,11 @@
+//bai 1
+//a
+let arr = [3, 451, 5, 2, 6, 76, 3, 5, 1, 243, 6, 4]
+
+let doDaiMang = arr.length
+
+for(let i = 0; i < doDaiMang; i = i + 1) {
+   console.log('phan tu thu %s = %s', i, arr[i])
+}
+//b
+let sum = 0
